989f3a4b16ebc5a753a653bd41749888763f58b8
Carlos Cogliandro <carloscogliandro22@gmail.com>
2022-11-17 20:40:33 +0100

    desafio 4

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 8080;
 let serverRoutes = require('./src/routes');
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors('*'));
 
 serverRoutes(app);
 
--- src/components/productos/index.js
+++ src/components/productos/index.js
@@ -1,18 +1,27 @@
 let express = require('express')
 let { Router } = require('express');
-
 let router = new Router();
 
+let addInfo = (req, res, next)=>{
+    req.body.Autoria = 'Carlos Cogliandro';
+    next();  
+}
+
 const Contenedor = require('../../contenedor/contenedor.js');
-const productos = new Contenedor(__dirname + '../../../data/productos.json');
+const productos = new Contenedor( __dirname + '../../../data/productos.json');
 
 
 module.exports = app => {
 
     let nuevoProducto = [];
 
-    app.use(express.static("public"));
     app.use('/api/productos', router);
+
+    router.use((req, res, next)=>{
+        req.body.router = true;
+        next();
+    });
+
 
     router.get('/', async (req, res, next)=>{
         return res.json(await productos.getAll());
@@ -21,9 +30,9 @@ module.exports = app => {
     router.get('/:id', (req, res) => {
         let id = Number(req.params.id);
         return res.json(productos.getById(id));
-      });
+    });
       
-    router.post('/', (req, res) => {
+    router.post('/', addInfo, (req, res) => {
         let obj = req.body;
         nuevoProducto.push(obj)        
         return res.json({productos: nuevoProducto});
